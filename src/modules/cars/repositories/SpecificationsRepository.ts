c984928d8081ff7b3dbba5d700e3f10c6576a689
Luís Miguel Silva Amorim <luismiguelsilvaamorim@gmail.com>
2021-12-19 01:53:03 -0300

    add SpecificationsController && refactoring classes

--- src/modules/cars/repositories/SpecificationsRepository.ts
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -5,8 +5,17 @@ class SpecificationsRepository implements ISpecificationsRepository{
 
     private specifications: Specification[];
 
-    constructor(){
+    private static INSTANCE: SpecificationsRepository;
+
+    private constructor(){
         this.specifications = [];
+    };
+
+    public static getInstance(){
+        if(!SpecificationsRepository.INSTANCE){
+            SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+        };
+        return SpecificationsRepository.INSTANCE;
     };
 
     public create({description, name}: ICreateSpecificationDTO): void{
