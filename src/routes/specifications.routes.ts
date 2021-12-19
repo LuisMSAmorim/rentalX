a30e8c1b037cdd307a76718959e1d86779be6b84
Luís Miguel Silva Amorim <luismiguelsilvaamorim@gmail.com>
2021-12-19 01:58:12 -0300

    fixing Specification create route

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,24 +1,17 @@
 import { Router } from "express";
 import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/useCases/createSpecification/CreateSpecificationUseCase";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const SpecificationsRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
 
 SpecificationsRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createspecificationService = new CreateSpecificationService(specificationsRepository);
-
-    createspecificationService.execute({ name, description });
-
-    return response.status(201).send();
+   return createSpecificationController.handle(request, response);
 });
 
-SpecificationsRoutes.get("/", (request, response) => {
-    const Specifications = specificationsRepository.list();
+// SpecificationsRoutes.get("/", (request, response) => {
+//     const Specifications = specificationsRepository.list();
 
-    return response.status(200).json({Specifications});
-});
+//     return response.status(200).json({Specifications});
+// });
 
 export { SpecificationsRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import express from 'express';
+import express, { Router } from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
 import { SpecificationsRoutes } from './routes/specifications.routes';
+import { router } from "./routes/index.routes";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', SpecificationsRoutes);
+app.use(Router);
 
 app.listen(3333, () => {
     console.log('Server is Running');
