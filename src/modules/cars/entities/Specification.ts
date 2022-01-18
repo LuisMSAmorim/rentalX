89d6441de43be173ee1adfdba8647edfee6af66c
Luís Miguel <luismiguelsilvaamorim@gmail.com>
2022-01-18 11:37:31 -0300

    create specifications table migration && fixing entity to typeorm

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,10 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 
+@Entity("specifications")
 class Specification{
     
+    @PrimaryColumn()
     private id: string;
+
+    @Column()
     public name: string;
+
+    @Column()
     public description: string;
+
+    @CreateDateColumn()
     private createdAt: Date;
 
     constructor(){
