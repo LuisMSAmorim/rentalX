caf2497e0105639366f6778d9487f9dd81993248
Luís Miguel <luismiguelsilvaamorim@gmail.com>
2022-01-25 14:23:24 -0300

    add file exclusion to updateUserAvatarUseCase

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -22,6 +23,10 @@ class UpdateUserAvatarUseCase {
     public async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(user_id);
 
+        if(user.avatar){
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        };
+        
         user.avatar = avatar_file;
 
         await this.usersRepository.create(user);
