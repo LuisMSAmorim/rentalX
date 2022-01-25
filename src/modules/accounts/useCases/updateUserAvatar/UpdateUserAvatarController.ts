a6718e67e185742162374c6c7213bbee6cdd3b47
Luís Miguel <luismiguelsilvaamorim@gmail.com>
2022-01-25 11:18:03 -0300

    add createAvatarUserController to users routes

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -6,11 +6,14 @@ import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 class UpdateUserAvatarController {
 
     public async handle(request: Request, response: Response){
-        const { user_id, avatar_file } = request.body;
+        const { id } = request.user;
+        const { avatar_file } = request.body;
 
         const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
-        await updateUserAvatarUseCase.execute({ user_id, avatar_file });
+        await updateUserAvatarUseCase.execute({ user_id: id, avatar_file });
+
+        return response.status(204).send();
     };
 };
 
