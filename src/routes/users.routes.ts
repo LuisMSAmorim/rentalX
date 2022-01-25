2c765dc7a6503a78eac0857b9543c6cc4592c1a6
Luís Miguel <luismiguelsilvaamorim@gmail.com>
2022-01-25 12:09:59 -0300

    add uploadAvatart middleware to /avatar route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import upload from "../config/upload";
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
 
-usersRoutes.patch('/avatar', updateUserAvatarController.handle);
+const uploadAvatar = multer(upload.upload("./tmp/avatart"));
+
+usersRoutes.patch('/avatar', uploadAvatar.single("avatar"), updateUserAvatarController.handle);
 usersRoutes.post('/', createUserController.handle);
 
 export { usersRoutes };
