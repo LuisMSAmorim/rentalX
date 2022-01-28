8ac12cb0d6d146c7e14837cd5a2db2b36ef2c355
Luís Miguel <luismiguelsilvaamorim@gmail.com>
2022-01-28 17:34:28 -0300

    fixing issues

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,7 +4,7 @@ import multer from "multer";
 import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreateCategoryController";
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
-import { ensureAuthenticated } from "middlewares/ensureAuthenticated";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const categoriesRoutes = Router();
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,7 +4,7 @@ import multer from "multer";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import upload from "@config/upload";
-import { ensureAuthenticated } from "middlewares/ensureAuthenticated";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 
