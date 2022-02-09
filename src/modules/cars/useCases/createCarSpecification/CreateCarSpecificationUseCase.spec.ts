d90cfa48a033622ca00322cd8c4c1083b83c831f
Luís Miguel <luismiguelsilvaamorim@gmail.com>
2022-02-09 17:44:07 -0300

    add createCarSpecification tests

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -1,5 +1,6 @@
 import { CreateCarSpecificationUseCase } from "./CreateCarSpecificationUseCase";
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 let carsRepository: CarsRepositoryInMemory;
 let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
@@ -12,6 +13,27 @@ describe("Create Car Specification", () => {
     });
 
     it("Should be able to add a new specification into a car", async () => {
-        
+        expect(async () => {
+            const car_id = "12345";
+            const specifications_id = ["54321"];
+    
+            await createCarSpecificationUseCase.execute({ car_id, specifications_id });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("Should not be able to add a new specification into a non-existent car", async () => {
+        const car = await carsRepository.create({
+            name: "Car2",
+            description: "A description",
+            daily_rate: 100,
+            license_plate: "WSL-1234",
+            fine_amount: 60,
+            brand: "Test",
+            category_id: "testing"
+        });
+
+        const specifications_id = ["54321"];
+
+        await createCarSpecificationUseCase.execute({ car_id: car.id, specifications_id });
     });
 });
