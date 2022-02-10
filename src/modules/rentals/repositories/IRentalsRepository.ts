81668d869d87945eea29761ba17d014802ad81a1
Luís Miguel <luismiguelsilvaamorim@gmail.com>
2022-02-10 17:19:56 -0300

    add rental Entity and IRepository

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,8 @@
+
+
+interface IRentalsRepository {
+    findOpenRentalByCar(car_id: string): Promise<Rental>;
+    findOpenRentalByUser(user_id: string): Promise<Rental>;
+};
+
+export { IRentalsRepository };
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,5 +1,8 @@
-import { injectable } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+
+import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
     user_id: string;
@@ -10,8 +13,28 @@ interface IRequest {
 @injectable()
 class CreateRentalUseCase {
 
-    public async execute(): Promise<void> {
+    private rentalsRepository: IRentalsRepository;
 
+    constructor(
+        @inject("RentalsRepository")
+        rentalsRepository:
+        RentalsRepository
+    ){
+        this.rentalsRepository = rentalsRepository;
+    };
+
+    public async execute({ car_id, expected_return_date, user_id }: IRequest): Promise<void> {
+        const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(car_id);
+
+        if(carUnavailable){
+            throw new AppError("Car isnt available");
+        };
+
+        const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(user_id);
+
+        if(rentalOpenToUser){
+            throw new AppError("User already have an open rental");
+        };
     };
 };
 
