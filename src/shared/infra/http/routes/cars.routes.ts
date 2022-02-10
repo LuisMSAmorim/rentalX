9d1ed13b29b8c0d9dd2f82f78fb38b8102c19c7b
Luís Miguel <luismiguelsilvaamorim@gmail.com>
2022-02-10 13:33:34 -0300

    add /specifications/:id route at cars.routes

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,4 +1,5 @@
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { Router } from "express";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
@@ -8,9 +9,12 @@ const carsRoutes = Router();
 
 let createCarController = new CreateCarController();
 let listAvailableCarsController = new ListAvailableCarsController();
+let createCarsSpecificationsController = new CreateCarSpecificationController();
 
 carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
 
 carsRoutes.get('/available', listAvailableCarsController.handle);
+
+carsRoutes.post('/specifications/:id', ensureAuthenticated, ensureAdmin, createCarsSpecificationsController.handle);
 
 export { carsRoutes };
