59e425b26122c47c4b9cf4746adc17c3c7db617b
Luís Miguel <luismiguelsilvaamorim@gmail.com>
2022-02-10 16:47:26 -0300

    add rentals migration

---
+++ src/shared/infra/typeorm/migrations/1644522080381-CreateRentals.ts
@@ -0,0 +1,73 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateRentals1644522080381 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "rentals",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "car_id",
+                        type: "uuid"
+                    },
+                    {
+                        name: "user_id",
+                        type: "uuid"
+                    },
+                    {
+                        name: "start_date",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "end_date",
+                        type: "timestamp"
+                    },
+                    {
+                        name: "total",
+                        type: "numeric"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: "FKcarRental",
+                        referencedColumnNames: ["id"],
+                        referencedTableName: "cars",
+                        columnNames: ["car_id"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    },
+                    {
+                        name: "FKuserRental",
+                        referencedColumnNames: ["id"],
+                        referencedTableName: "users",
+                        columnNames: ["user_id"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    }, 
+                ]
+            })
+        )
+    };
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("rentals");
+    };
+
+};
