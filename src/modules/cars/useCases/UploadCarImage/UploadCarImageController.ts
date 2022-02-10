d63d7e67db8727a833b82d592881bf7569c8ba4d
Luís Miguel <luismiguelsilvaamorim@gmail.com>
2022-02-10 14:23:50 -0300

    add uploadCarImageUseCase

---
+++ src/modules/cars/useCases/UploadCarImage/UploadCarImageController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+interface IFiles {
+    filename: string;
+};
+
+import { UploadCarImageUseCase } from "./UploadCarImageUseCase";
+
+class UploadCarImageController {
+
+    public async handle(request: Request, response: Response): Promise<Response> {
+        const { id } = request.params;
+        const images = request.files as IFiles[];
+
+        const uploadCarImageUseCase = container.resolve(UploadCarImageUseCase);
+
+        const images_name = images.map(file => file.filename);
+
+        await uploadCarImageUseCase.execute({ car_id: id, images_name });
+
+        return response.status(201).send();
+    };
+};
+
+export { UploadCarImageController };
--- src/modules/cars/useCases/UploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/UploadCarImage/UploadCarImageUseCase.ts
@@ -1,7 +1,31 @@
+import { inject, injectable } from "tsyringe";
 
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImageRepository";
 
+interface IRequest {
+    car_id: string;
+    images_name: string[];
+};
+
+@injectable()
 class UploadCarImageUseCase {
 
+    private carsImagesRepository: ICarsImagesRepository;
+    
+    constructor(
+        @inject("CarsImagesRepository")
+        carsImagesRepository:
+        ICarsImagesRepository
+    ){
+        this.carsImagesRepository = carsImagesRepository;
+    };
+
+    public async execute({ car_id, images_name }: IRequest): Promise<void> {
+
+        images_name.map(async (image) => {
+            await this.carsImagesRepository.create(car_id, image);
+        });
+    };
 };
 
 export { UploadCarImageUseCase };
